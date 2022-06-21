40801f603b877f6e3a4262d0f1ab714cbf9a58af
rajponna <rajponna@publicisgroupe.net>
2022-06-21 20:06:25 +0100

    inspec test

--- controls/gcp_cloudsql.rb
+++ controls/gcp_cloudsql.rb
@@ -25,3 +25,17 @@ control "gcp-regions-loop-1.0" do                                               
     end
   end
 end
+
+control "gcp-cloud-sql-1.0" do
+  impact 1.0
+  title "Ensure cloud sql"
+  desc "An optional description..."
+  google_sql_database_instances(project:gcp_project_id).instance_names.each do |instance_name|
+    instance = google_sql_database_instance(project: gcp_project_id, database: instance_name)
+
+    describe "Instance name $#{instance.name}$ of cloud sql" do
+      subject{instance.settings.availability_type}
+      it { should cmp 'REGIONAL' }
+    end
+  end
+end
